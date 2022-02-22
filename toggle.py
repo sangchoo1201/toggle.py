24472391574f1ca96535c5dd4cb367cf742c82cf
sangchoo1201 <sanghyuk1201@gmail.com>
2022-02-22 11:11:26 +0900

    v0.3.2
    파일 서식 다시 지정

--- toggle.py
+++ toggle.py
@@ -1,7 +1,7 @@
 import random
 import pygame
 import win32clipboard as clip
-from typing import List
+from typing import List, Tuple, Optional
 import base64
 
 clock = pygame.time.Clock()
@@ -15,180 +15,209 @@ pygame.init()
 
 pygame.display.set_caption("Toggle.py")
 
-level_code: List = [[], [], []]
+level_code: list = [[], [], []]
 x_max, y_max = 0, 0
 
 
-def flip(x, y):
+def flip(x: int, y: int) -> None:
     level_code[2][y][x] ^= 1
 
-def flip_u(x, y):
-    for dy in range(y+1):
+
+def flip_u(x: int, y: int) -> None:
+    for dy in range(y + 1):
         level_code[2][dy][x] ^= 1
 
-def flip_d(x, y):
+
+def flip_d(x: int, y: int) -> None:
     for dy in range(y, y_max):
         level_code[2][dy][x] ^= 1
 
-def flip_l(x, y):
-    for dx in range(x+1):
+
+def flip_l(x: int, y: int) -> None:
+    for dx in range(x + 1):
         level_code[2][y][dx] ^= 1
 
-def flip_r(x, y):
+
+def flip_r(x: int, y: int) -> None:
     for dx in range(x, x_max):
         level_code[2][y][dx] ^= 1
 
-def flip_lu(x, y):
+
+def flip_lu(x: int, y: int) -> None:
     while x >= 0 and y >= 0:
         level_code[2][y][x] ^= 1
-        x, y = x-1, y-1
+        x, y = x - 1, y - 1
 
-def flip_ru(x, y):
+
+def flip_ru(x: int, y: int) -> None:
     while x < x_max and y >= 0:
         level_code[2][y][x] ^= 1
-        x, y = x+1, y-1
+        x, y = x + 1, y - 1
 
-def flip_rd(x, y):
+
+def flip_rd(x: int, y: int) -> None:
     while x < x_max and y < y_max:
         level_code[2][y][x] ^= 1
-        x, y = x+1, y+1
+        x, y = x + 1, y + 1
 
-def flip_ld(x, y):
+
+def flip_ld(x: int, y: int) -> None:
     while x >= 0 and y < y_max:
         level_code[2][y][x] ^= 1
-        x, y = x-1, y+1
+        x, y = x - 1, y + 1
 
-def flip_l_r(x, y):
+
+def flip_l_r(x: int, y: int) -> None:
     flip_l(x, y)
     flip_r(x, y)
     flip(x, y)
 
-def flip_u_d(x, y):
+
+def flip_u_d(x: int, y: int) -> None:
     flip_u(x, y)
     flip_d(x, y)
     flip(x, y)
 
-def flip_lu_rd(x, y):
+
+def flip_lu_rd(x: int, y: int) -> None:
     flip_lu(x, y)
     flip_rd(x, y)
     flip(x, y)
 
-def flip_ru_ld(x, y):
+
+def flip_ru_ld(x: int, y: int) -> None:
     flip_ru(x, y)
     flip_ld(x, y)
     flip(x, y)
 
-def rotate_cw(x, y):
-    positions = ((-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0))
-    li = []
-    for dx, dy in positions:
-        if not (0 <= x+dx < x_max and 0 <= y+dy < y_max):
-            li.append(0)
-        else:
-            li.append(level_code[2][y+dy][x+dx])
+
+def rotate_cw(x: int, y: int) -> None:
+    positions = (
+        (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)
+    )
+    li = [
+        level_code[2][y + dy][x + dx]
+        if 0 <= x + dx < x_max and 0 <= y + dy < y_max
+        else 0
+        for dx, dy in positions
+    ]
     li.insert(0, li.pop())
     for i, d in enumerate(positions):
         dx, dy = d
-        if 0 <= x+dx < x_max and 0 <= y+dy < y_max:
-            level_code[2][y+dy][x+dx] = li[i]
+        if 0 <= x + dx < x_max and 0 <= y + dy < y_max:
+            level_code[2][y + dy][x + dx] = li[i]
     flip(x, y)
 
-def rotate_ccw(x, y):
-    positions = ((-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0))
-    li = []
-    for dx, dy in positions:
-        if not (0 <= x+dx < x_max and 0 <= y+dy < y_max):
-            li.append(0)
-        else:
-            li.append(level_code[2][y+dy][x+dx])
+
+def rotate_ccw(x: int, y: int) -> None:
+    positions = (
+        (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)
+    )
+    li = [
+        level_code[2][y + dy][x + dx]
+        if 0 <= x + dx < x_max and 0 <= y + dy < y_max
+        else 0
+        for dx, dy in positions
+    ]
     li.append(li[0])
     del li[0]
     for i, d in enumerate(positions):
         dx, dy = d
-        if 0 <= x+dx < x_max and 0 <= y+dy < y_max:
-            level_code[2][y+dy][x+dx] = li[i]
+        if 0 <= x + dx < x_max and 0 <= y + dy < y_max:
+            level_code[2][y + dy][x + dx] = li[i]
     flip(x, y)
 
-def flip_diamond(x, y):
+
+def flip_diamond(x: int, y: int) -> None:
     flip_r(x, y)
     flip_l(x, y)
     flip_u(x, y)
     flip_d(x, y)
     flip(x, y)
 
-def flip_square(x, y):
-    positions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1))
+
+def flip_square(x: int, y: int) -> None:
+    positions = (
+        (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)
+    )
     for dx, dy in positions:
         if 0 <= x + dx < x_max and 0 <= y + dy < y_max:
-            flip(x+dx, y+dy)
+            flip(x + dx, y + dy)
 
-def mirror_lr(x, y):
+
+def mirror_lr(x: int, y: int) -> None:
     for dy in range(y_max):
-        a = 0 if x == 0 else level_code[2][dy][x-1]
-        b = 0 if x == x_max-1 else level_code[2][dy][x+1]
+        a = 0 if x == 0 else level_code[2][dy][x - 1]
+        b = 0 if x == x_max - 1 else level_code[2][dy][x + 1]
         if x > 0:
-            level_code[2][dy][x-1] = b
-        if x < x_max-1:
-            level_code[2][dy][x+1] = a
+            level_code[2][dy][x - 1] = b
+        if x < x_max - 1:
+            level_code[2][dy][x + 1] = a
     flip(x, y)
 
-def mirror_ud(x, y):
+
+def mirror_ud(x: int, y: int) -> None:
     for dx in range(x_max):
-        a = 0 if y == 0 else level_code[2][y-1][dx]
-        b = 0 if y == y_max-1 else level_code[2][y+1][dx]
+        a = 0 if y == 0 else level_code[2][y - 1][dx]
+        b = 0 if y == y_max - 1 else level_code[2][y + 1][dx]
         if y > 0:
-            level_code[2][y-1][dx] = b
-        if y < y_max-1:
-            level_code[2][y+1][dx] = a
+            level_code[2][y - 1][dx] = b
+        if y < y_max - 1:
+            level_code[2][y + 1][dx] = a
     flip(x, y)
+
 
 # noinspection PyUnusedLocal
-def shift_l(x, y):
+def shift_l(x: int, y: int) -> None:
     row = level_code[2][y]
     row.append(row[0])
     del row[0]
     level_code[2][y] = row
 
+
 # noinspection PyUnusedLocal
-def shift_r(x, y):
+def shift_r(x: int, y: int) -> None:
     row = level_code[2][y]
     row.insert(0, row.pop())
     level_code[2][y] = row
 
 
-def click(x, y, reverse=False):
+def click(x: int, y: int, reverse: Optional[bool] = False) -> None:
     global do
 
-    functions = (flip, flip_u, flip_d, flip_l, flip_r,
-                 flip_lu, flip_ru, flip_rd, flip_ld,
-                 flip_l_r, flip_u_d, flip_lu_rd, flip_ru_ld,
-                 rotate_cw, rotate_ccw, flip_diamond, flip_square,
-                 mirror_lr, mirror_ud, shift_l, shift_r)
+    functions = (
+        flip, flip_u, flip_d, flip_l, flip_r,
+        flip_lu, flip_ru, flip_rd, flip_ld,
+        flip_l_r, flip_u_d, flip_lu_rd, flip_ru_ld,
+        rotate_cw, rotate_ccw, flip_diamond, flip_square,
+        mirror_lr, mirror_ud, shift_l, shift_r
+    )
 
     num = level_code[1][y][x]
     if num == 0:
         return
 
-    if reverse:
-        if num in (13, 19):
-            num += 1
-        elif num in (14, 20):
-            num -= 1
+    if reverse and num in (13, 19):
+        num += 1
+    elif reverse and num in (14, 20):
+        num -= 1
+
     functions[num](x, y)
     if not reverse:
         do.append((x, y))
 
-def pos():
+
+def pos() -> Tuple[int, int]:
     x, y = pygame.mouse.get_pos()
     tile_size = round(minimum * 0.7 / max(level_code[0]))
-    x -= round(screen_width/2 + (-(x_max-1)/2 - 0.5) * tile_size)
+    x -= round(screen_width / 2 + (-(x_max - 1) / 2 - 0.5) * tile_size)
     x //= tile_size
-    y -= round(screen_height/2 + (-(y_max-1)/2 - 0.5) * tile_size)
+    y -= round(screen_height / 2 + (-(y_max - 1) / 2 - 0.5) * tile_size)
     y //= tile_size
     return x, y
 
 
-def undo():
+def undo() -> None:
     global do, did, click_count
 
     if not do:
@@ -200,7 +229,8 @@ def undo():
     click(*move, True)
     click_count -= 1
 
-def redo():
+
+def redo() -> None:
     global did, click_count
 
     if not did:
@@ -210,7 +240,8 @@ def redo():
     click(*move)
     click_count += 1
 
-def reset():
+
+def reset() -> None:
     global do, did, click_count
 
     while do:
@@ -219,16 +250,17 @@ def reset():
     click_count = 0
 
 
-def print_state():  # for debugging
+def print_state() -> None:  # for debugging
     for row in level_code[2]:
         print(*row)
 
-def print_shape():  # for debugging
+
+def print_shape() -> None:  # for debugging
     for row in level_code[1]:
         print(*row)
 
 
-def generate(width, height, click_amount, shape_list):  # sourcery no-metrics
+def generate(width: int, height: int, click_amount: int, shape_list: List[int]) -> None:
     global x_max, y_max, do, did, click_count
 
     level_code[0] = tuple((width, height))
@@ -245,19 +277,15 @@ def generate(width, height, click_amount, shape_list):  # sourcery no-metrics
                 if shape is not None:
                     discard_list.append(shape)
                 shape = shape_list.pop() if shape_list else random.randint(0, 16)
-                if j == 0 and shape in (3, 5, 8):
-                    continue
-                if j == width - 1 and shape in (4, 6, 7):
-                    continue
-                if i == 0 and shape in (1, 5, 6):
-                    continue
-                if i == height - 1 and shape in (2, 7, 8):
-                    continue
-                if (j, i) in ((0, height-1), (width-1, 0)) and shape == 11:
-                    continue
-                if (j, i) in ((0, 0), (width-1, height-1)) and shape == 12:
-                    continue
-                break
+                if not any((
+                        j == 0 and shape in (3, 5, 8),
+                        j == width - 1 and shape in (4, 6, 7),
+                        i == 0 and shape in (1, 5, 6),
+                        i == height - 1 and shape in (2, 7, 8),
+                        (j, i) in ((0, height - 1), (width - 1, 0)) and shape == 11,
+                        (j, i) in ((0, 0), (width - 1, height - 1)) and shape == 12
+                )):
+                    break
             level_code[1][i][j] = shape
 
     level_code[2] = [[0 for _ in range(width)][:] for _ in range(height)]
@@ -265,26 +293,28 @@ def generate(width, height, click_amount, shape_list):  # sourcery no-metrics
     random.shuffle(li)
     for _ in range(click_amount):
         i = li.pop()
-        x, y = i%width, i//width
+        x, y = i % width, i // width
         while level_code[1][y][x] == 0:
             i = li.pop()
-            x, y = i%width, i//width
+            x, y = i % width, i // width
         click(x, y, True)
     do, did = [], []
     click_count = 0
 
-def v1_encode(author, name):  # unused function
+
+def v1_encode(author: str, name: str) -> str:  # unused function
     encoded_text = "".join(
         str(i) + spliter
         for i in (author, name, level_code[0][0], level_code[0][1])
     )
 
     for i, j in zip(sum(level_code[1], []), sum(level_code[2], [])):
-        char = 64 + (j^1)*32 + i
+        char = 64 + (j ^ 1) * 32 + i
         encoded_text += chr(char)
-    return base64.b64encode(bytes(encoded_text))
+    return base64.b64encode(bytes(encoded_text)).decode()
 
-def v1_decode():
+
+def v1_decode() -> None:
     global level_code, x_max, y_max, code
 
     x_max, y_max = codes[2], codes[3]
@@ -300,31 +330,34 @@ def v1_decode():
             level_code[2][i][j] = 0 if (a & 32) else 1
 
 
-def draw():
-    imgs = (0, "u", "d", "l", "r",
-            "lu", "ru", "rd", "ld",
-            "l_r", "u_d", "lu_rd", "ru_ld",
-            "cw", "ccw", "diamond", "square",
-            "vertical", "horizontal", "shl", "shr")
+def draw() -> None:
+    imgs = (
+        "", "u", "d", "l", "r",
+        "lu", "ru", "rd", "ld",
+        "l_r", "u_d", "lu_rd", "ru_ld",
+        "cw", "ccw", "diamond", "square",
+        "vertical", "horizontal", "shl", "shr"
+    )
     gray = (127, 127, 127)
     yellow = (255, 255, 0)
     tile_size = round(min(screen_width, screen_height) * 0.7 / max(level_code[0]))
     for x in range(x_max):
         for y in range(y_max):
-            dx = round(screen_width/2 + (x - (x_max-1)/2 - 0.5) * tile_size)
-            dy = round(screen_height/2 + (y - (y_max-1)/2 - 0.5) * tile_size)
+            dx = round(screen_width / 2 + (x - (x_max - 1) / 2 - 0.5) * tile_size)
+            dy = round(screen_height / 2 + (y - (y_max - 1) / 2 - 0.5) * tile_size)
             color = yellow if level_code[2][y][x] else gray
-            rect = (dx+1, dy+1, tile_size-2, tile_size-2)
+            rect = (dx + 1, dy + 1, tile_size - 2, tile_size - 2)
             pygame.draw.rect(screen, color, rect)
 
             if level_code[1][y][x] == 0:
                 continue
             src = f"img/{imgs[level_code[1][y][x]]}.png"
             img = pygame.image.load(src)
-            img = pygame.transform.scale(img, (tile_size//3*2, tile_size//3*2))
-            screen.blit(img, (dx+tile_size//6, dy+tile_size//6))
+            img = pygame.transform.scale(img, (tile_size // 3 * 2, tile_size // 3 * 2))
+            screen.blit(img, (dx + tile_size // 6, dy + tile_size // 6))
 
-def draw_text(value, size, positions):
+
+def draw_text(value: str, size: int, positions: Tuple[int, int]) -> pygame.rect:
     font = pygame.font.SysFont("consolas", size)
     img = font.render(value, True, (255, 255, 255))
     rect = img.get_rect()
@@ -333,7 +366,7 @@ def draw_text(value, size, positions):
     return rect
 
 
-def set_mode(select):
+def set_mode(select: str) -> None:
     global mode, click_count, amount, level, do, did, run, code, codes
 
     mode = select
@@ -359,7 +392,7 @@ def set_mode(select):
             code = codes[4]
             for i in range(64, 85):
                 codes[4] = codes[4].replace(chr(i), "")
-                codes[4] = codes[4].replace(chr(i+32), "")
+                codes[4] = codes[4].replace(chr(i + 32), "")
             assert not codes[4]
             codes.pop()
         except:
@@ -370,11 +403,15 @@ def set_mode(select):
         run = False
 
 
-mode, click_count, amount, level, do, did, code, codes = [None]*8
+mode, click_count, amount, level, do, did, code, codes = [None] * 8
 selection_max = 3
-modes = ["classic", "custom play", "exit"]
+modes = [
+    "classic", "custom play", "exit"
+]
 spliter = ":"
-classic_shape = [0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+classic_shape = [
+    0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16
+]
 set_mode("menu")
 
 run = True
@@ -390,10 +427,10 @@ while run:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 px, py = pygame.mouse.get_pos()
-                if not screen_width//2 - minimum//6 <= px <= screen_width//2 + minimum//6:
+                if not screen_width // 2 - minimum // 6 <= px <= screen_width // 2 + minimum // 6:
                     continue
-                py -= screen_height//2
-                py = round(py/(minimum//8))
+                py -= screen_height // 2
+                py = round(py / (minimum // 8))
                 if 0 <= py < selection_max:
                     set_mode(modes[py])
             if event.type != pygame.KEYDOWN:
@@ -402,21 +439,49 @@ while run:
                 run = False
 
         logo = "TOGGLE.py"
-        draw_text(logo, minimum//10, (screen_width//2, screen_height//2 - minimum//3))
+        draw_text(logo, minimum // 10, (screen_width // 2, screen_height // 2 - minimum // 3))
         for index, text in enumerate(modes):
-            position = (screen_width//2, screen_height//2 + minimum//8*index)
-            draw_text(text, minimum//15, position)
+            position = (screen_width // 2, screen_height // 2 + minimum // 8 * index)
+            draw_text(text, minimum // 15, position)
 
     elif mode == "custom play":
-        return_rect = draw_text("return to menu", minimum//20, (screen_width//2, screen_height//2 + minimum//5))
+        return_rect = draw_text(
+            "return to menu",
+            minimum // 20,
+            (screen_width // 2, screen_height // 2 + minimum // 5)
+        )
         if code:
-            draw_text(f"{codes[1]}", minimum//15, (screen_width//2, screen_height//2 - minimum//4))
-            draw_text(f"by {codes[0]}", minimum//20, (screen_width//2, screen_height//2 - round(minimum*0.17)))
-            draw_text(f"size: {codes[2]}*{codes[3]}", minimum//20, (screen_width//2, screen_height//2))
-            play_rect = draw_text("play!", minimum//20, (screen_width//2, screen_height//2 + round(minimum*0.3)))
+            draw_text(
+                f"{codes[1]}",
+                minimum // 15,
+                (screen_width // 2, screen_height // 2 - minimum // 4)
+            )
+            draw_text(
+                f"by {codes[0]}",
+                minimum // 20,
+                (screen_width // 2, screen_height // 2 - round(minimum * 0.17))
+            )
+            draw_text(
+                f"size: {codes[2]}*{codes[3]}",
+                minimum // 20,
+                (screen_width // 2, screen_height // 2)
+            )
+            play_rect = draw_text(
+                "play!",
+                minimum // 20,
+                (screen_width // 2, screen_height // 2 + round(minimum * 0.3))
+            )
         else:
-            draw_text("No valid level", minimum//15, (screen_width//2, screen_height//2 - minimum//10))
-            draw_text("in your clipboard!", minimum//15, (screen_width//2, screen_height//2))
+            draw_text(
+                "No valid level",
+                minimum // 15,
+                (screen_width // 2, screen_height // 2 - minimum // 10)
+            )
+            draw_text(
+                "in your clipboard!",
+                minimum // 15,
+                (screen_width // 2, screen_height // 2)
+            )
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -445,11 +510,13 @@ while run:
                         did = []
                         click_count += 1
                     continue
-                elif not round((screen_height + minimum*0.78)/2) <= my <= round((screen_height + minimum*0.92)/2):
+                elif not round((screen_height + minimum * 0.78) / 2) \
+                        <= my \
+                        <= round((screen_height + minimum * 0.92) / 2):
                     continue
                 for index, func in enumerate((undo, redo, reset)):
-                    center_x = round((screen_width/2)-(minimum/3.5)*(1-index))
-                    if center_x - minimum*0.07 <= mx <= center_x + minimum*0.07:
+                    center_x = round((screen_width / 2) - (minimum / 3.5) * (1 - index))
+                    if center_x - minimum * 0.07 <= mx <= center_x + minimum * 0.07:
                         func()
                         break
 
@@ -475,12 +542,20 @@ while run:
                 set_mode("menu")
 
         score = f"level:{level}  click:{click_count}"
-        position = (round(screen_width/2), round((screen_height-minimum*0.85)/2))
+        position = (
+            round(screen_width / 2),
+            round((screen_height - minimum * 0.85) / 2)
+        )
         draw_text(score, minimum // 20, position)
 
-        texts = ("undo", "redo", "reset")
+        texts = (
+            "undo", "redo", "reset"
+        )
         for index, text in enumerate(texts):
-            position = (round((screen_width/2)-(minimum/3.5)*(1-index)), round((screen_height + minimum*0.85)/2))
+            position = (
+                round((screen_width / 2) - (minimum / 3.5) * (1 - index)),
+                round((screen_height + minimum * 0.85) / 2)
+            )
             draw_text(text, minimum // 20, position)
 
     pygame.display.flip()
